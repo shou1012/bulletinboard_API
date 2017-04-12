c0053282a5968a8912e768d31a95d5fc00b6feb2
shohan ki <sho1012.seikyo@gmail.com>
2017-04-12 16:24:35 +0900

    users_controller.rb added update, destroy, show

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -1,9 +1,12 @@
 module V1
   class UsersController < ApplicationController
     skip_before_action :authenticate_user_from_token!, only: [:create]
+    before_action :set_user, only: [:update, :destroy, :show]
+    
+    def show
+      render json: @user
+    end
 
-    # POST
-    # Create an user
     def create
       @user = User.new user_params
 
@@ -14,10 +17,33 @@ module V1
       end
     end
 
-    private
+    def update
+      if @user.update(user_params)
+        render json: @user
+      else
+        render json: { error: t('user_update_error') }, status: :unprocessable_entity
+      end
+    end
 
+    def destroy
+      if @user.destroy
+        head :no_content
+      else
+        render json: { error: t('user_delete_error') }, status: :unprocessable_entity
+      end
+    end
+
+    private
     def user_params
       params.require(:user).permit(:email, :password)
+    end
+
+    def set_user
+      begin
+        @user = User.find(params[:id])
+      rescue ActiveRecord::RecordNotFound => e
+        render status: :not_found, json: { errors: "User not found" }
+      end
     end
   end
 end
